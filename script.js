52ed9cea85869a1de0739c4a1b9a8e6ced8f123c
James Stott <japstott@gmail.com>
2024-01-30 14:16:26 +0000

    create search functionality

--- script.js
+++ script.js
@@ -5,6 +5,10 @@ var geoApiURLPrefix = 'http://api.openweathermap.org/geo/1.0/direct?';
 var currentWeatherApiURLPrefix = 'https://api.openweathermap.org/data/2.5/weather?';
 var forecastWeatherApiURLPrefix = 'https://api.openweathermap.org/data/2.5/forecast?';
 
+var hourForDailyTemperature = 12;
+
+var apiError = '';
+
 async function getLatAndLonByLocationName(location) {
     // instantiate return object
     var coordinates = {};
@@ -18,28 +22,28 @@ async function getLatAndLonByLocationName(location) {
     var url = geoApiURLPrefix + [queryString, appId].join('&');
 
     // Attempt to perform fetch request on URL. If this returns a 404, despite being caught, Google Chrome will display this error in the browser
-    try {
-        var response = await fetch(url);
 
-        if (!response.ok) {
-            throw new Error(response.statusText);
-        }
+    var response = await fetch(url);
 
-        var locations = await response.json();
-
-        if (locations.length === 0) {
-            throw new Error(`Location ${location} was not recognised.`)
-        }
-
-        var { lat, lon } = locations[0];
-
-        coordinates.lat = lat;
-        coordinates.lon = lon;
-
-        return coordinates;
-    } catch (error) {
-        throw error;
+    if (!response.ok) {
+        updateApiError('An API Error occurred. Please try again later.');
+        throw new Error(response.statusText);
     }
+
+    var locations = await response.json();
+
+    if (locations.length === 0) {
+        updateApiError('Location not found.');
+        throw new Error(`Location ${location} was not recognised.`)
+    }
+
+    var { lat, lon } = locations[0];
+
+    coordinates.lat = lat;
+    coordinates.lon = lon;
+
+    return coordinates;
+
 }
 
 async function getCurrentWeatherData(lat, lon) {
@@ -49,26 +53,22 @@ async function getCurrentWeatherData(lat, lon) {
     // construct url using user provided latitude and longitude
     var url = currentWeatherApiURLPrefix + `lat=${lat}&lon=${lon}&appid=${apiKey}`;
 
-    try {
-        var response = await fetch(url);
+    var response = await fetch(url);
 
-        if (!response.ok) {
-            throw new Error(response.statusText);
-        }
-
-        var weatherData = await response.json();
-
-        console.log(weatherData);
-
-        currentWeatherData.temperature = convertTemperatureInKtoC(weatherData.main.temp); // API returns weather in Kelvin so we convert to Celsius using a helper function before returning
-        currentWeatherData.wind = convertMpsToKph(weatherData.wind.speed); // API returns m/s so we convert to kph first
-        currentWeatherData.humidity = weatherData.main.humidity;
-        currentWeatherData.weatherIcon = weatherData.weather.icon;
-
-        return currentWeatherData;
-    } catch (error) {
-        throw error;
+    if (!response.ok) {
+        updateApiError('An API Error occurred. Please try again later.');
+        throw new Error(response.statusText);
     }
+
+    var weatherData = await response.json();
+
+    currentWeatherData.temperature = convertTemperatureInKtoC(weatherData.main.temp); // API returns weather in Kelvin so we convert to Celsius using a helper function before returning
+    currentWeatherData.wind = convertMpsToKph(weatherData.wind.speed); // API returns m/s so we convert to kph first
+    currentWeatherData.humidity = weatherData.main.humidity;
+    currentWeatherData.weatherIcon = weatherData.weather.icon;
+
+    return currentWeatherData;
+
 }
 
 async function get5DayForecast(lat, lon) {
@@ -77,19 +77,16 @@ async function get5DayForecast(lat, lon) {
     // construct url
     var url = forecastWeatherApiURLPrefix + `lat=${lat}&lon=${lon}&appid=${apiKey}`;
 
-    try {
-        var response = await fetch(url);
+    var response = await fetch(url);
 
-        if (!response.ok) {
-            throw new Error(response.statusText);
-        }
-
-        var forecastData = await response.json();
-
-        return forecastData;
-    } catch (error) {
-        throw error;
+    if (!response.ok) {
+        updateApiError('An API Error occurred. Please try again later.');
+        throw new Error(response.statusText);
     }
+
+    var forecastData = await response.json();
+
+    return forecastData;
 }
 
 function convertTemperatureInKtoC(temperature) {
@@ -127,11 +124,49 @@ function getDailyForecastAtHour(forecast, hour) {
     return dailyForecast;
 }
 
+async function searchForWeatherByLocation(location) {
+    apiError = '';
+
+    try {
+        var locationCoordinates = await getLatAndLonByLocationName(location);
+
+        getCurrentWeatherData(locationCoordinates.lat, locationCoordinates.lon).then((weatherData) => {
+            updateWeatherUI(weatherData);
+        });
+
+        get5DayForecast(locationCoordinates.lat, locationCoordinates.lon).then((forecastData) => {
+            var forecastSummary = getDailyForecastAtHour(forecastData, hourForDailyTemperature);
+            updateForecastUI(forecastSummary);
+        });
+
+    } catch (error) {
+
+    }
+
+    console.log(apiError);
+}
+
+function updateApiError(newError) {
+    if (apiError === '') {
+        apiError = newError;
+    }
+}
+
+function updateWeatherUI(weatherData) {
+
+}
+
+function updateForecastUI(forecastData) {
+
+}
+
 (async () => {
     var londonCoordinates = await getLatAndLonByLocationName('London');
     var currentWeather = await getCurrentWeatherData(londonCoordinates.lat, londonCoordinates.lon);
     var fiveDayForecast = await get5DayForecast(londonCoordinates.lat, londonCoordinates.lon);
 
     getDailyForecastAtHour(fiveDayForecast, 12);
+
+    searchForWeatherByLocation('Location')
 })();
 
