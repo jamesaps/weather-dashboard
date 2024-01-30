6f12c1ba835c635e921025d93c561702ecff2b20
James Stott <japstott@gmail.com>
2024-01-30 02:55:50 +0000

    create functionality to get current weather data from latitude and longitude

--- script.js
+++ script.js
@@ -2,6 +2,7 @@
 var apiKey = '60138034af71780e3420402cea540efb';
 
 var geoApiURLPrefix = 'http://api.openweathermap.org/geo/1.0/direct?';
+var currentWeatherApiURLPrefix = 'https://api.openweathermap.org/data/2.5/weather';
 
 async function getLatAndLonByLocationName(location) {
     // instantiate return object
@@ -29,7 +30,7 @@ async function getLatAndLonByLocationName(location) {
             throw new Error(`Location ${location} was not recognised.`)
         }
 
-        var { lat, lon } = locations[0]
+        var { lat, lon } = locations[0];
 
         coordinates.lat = lat;
         coordinates.lon = lon;
@@ -41,4 +42,47 @@ async function getLatAndLonByLocationName(location) {
     }
 }
 
-getLatAndLonByLocationName('London')
+async function getCurrentWeatherData(lat, lon) {
+    // instantiate current weather data return object
+    var currentWeatherData = {};
+
+    // construct url using user provided latitude and longitude
+    var url = currentWeatherApiURLPrefix + `?lat=${lat}&lon=${lon}&appid=${apiKey}`;
+
+    try {
+        var response = await fetch(url);
+
+        if (!response.ok) {
+            throw new Error(response.statusText);
+        }
+
+        var weatherData = await response.json();
+
+        console.log(weatherData);
+
+        currentWeatherData.temperature = convertTemperatureInKtoC(weatherData.main.temp); // API returns weather in Kelvin so we convert to Celsius using a helper function before returning
+        currentWeatherData.wind = convertMpsToKph(weatherData.wind.speed); // API returns m/s so we convert to kph first
+        currentWeatherData.humidity = weatherData.main.humidity;
+        currentWeatherData.weatherIcon = weatherData.weather.icon;
+
+        return currentWeatherData;
+    } catch (error) {
+        alert(error);
+    }
+}
+
+function convertTemperatureInKtoC(temperature) {
+    return temperature - 273.15;
+}
+
+function convertMpsToKph(mps) {
+    // convert meters per second to kilometers per hour
+
+    return mps * 3.6;
+}
+
+(async () => {
+    var londonCoordinates = await getLatAndLonByLocationName('London');
+    var currentWeatherData = getCurrentWeatherData(londonCoordinates.lat, londonCoordinates.lon);
+})();
+
